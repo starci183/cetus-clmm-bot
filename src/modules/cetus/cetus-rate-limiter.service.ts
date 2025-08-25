1e72cfa894a2b32a48559bc22a8d63f3feaf6be6
starci183 <cuongnvtse160875@gmail.com>
2025-08-26 06:02:35 +0700

    123123

--- src/modules/cetus/cetus-rate-limiter.service.ts
+++ src/modules/cetus/cetus-rate-limiter.service.ts
@@ -1,20 +1,29 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, Logger } from "@nestjs/common"
+import { Interval } from "@nestjs/schedule"
 import { RateLimiterMemory } from "rate-limiter-flexible"
 
+const TX_COUNT_KEY = "cetus"
 @Injectable()
 export class CetusTxRateLimiterService {
+    private readonly logger = new Logger(CetusTxRateLimiterService.name)
     private limter = new RateLimiterMemory({
         points: 60,
         duration: 8 * 60 * 60
     })
     constructor() {}
 
+    @Interval(1000)
+    async logTxCount() {
+        const result = await this.limter.get(TX_COUNT_KEY)
+        this.logger.warn(`Tx count: ${result?.consumedPoints}`)
+    }
+
     public async increaseTxCount() {
-        await this.limter.consume("cetus")
+        await this.limter.consume(TX_COUNT_KEY)
     }
 
     public async isRateLimitExceeded() {
-        const result = await this.limter.get("cetus")
+        const result = await this.limter.get(TX_COUNT_KEY)
         return result ? result.remainingPoints <= 0 : false
     }
 }
