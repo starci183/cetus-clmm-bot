1b97f079e10d2bf303e11adbbed05c7c6df65e5f
starci183 <cuongnvtse160875@gmail.com>
2025-09-01 23:09:05 +0700

    123123

--- src/modules/cetus/swap.service.ts
+++ src/modules/cetus/swap.service.ts
@@ -17,16 +17,25 @@ import { BalanceManagerService } from "./balance-manager.service"
 import BN from "bn.js"
 import { CetusTxRateLimiterService } from "./cetus-rate-limiter.service"
 import { sleep } from "@/modules/common"
+
 export interface SwapParams {
-  profilePair: ProfilePairSchema;
-  amount?: number;
-  a2b: boolean;
-  slippage?: number;
+  profilePair: ProfilePairSchema
+  amount?: number
+  a2b: boolean
+  slippage?: number
+}
+
+export interface SwapResult {
+  balanceEnough: boolean
+  routeFound?: boolean
+  digest?: string
+  error?: string
 }
 
 @Injectable()
 export class CetusSwapService {
     private readonly logger = new Logger(CetusSwapService.name)
+
     constructor(
     @InjectSuiClient()
     private readonly suiClient: SuiClient,
@@ -46,7 +55,7 @@ export class CetusSwapService {
         if (!token) {
             throw new Error(`Token ${tokenId} not found`)
         }
-        return new BN(1).mul(new BN(10).pow(new BN(token.decimals))) // 1
+        return new BN(10).pow(new BN(token.decimals)) // 1 unit of token
     }
 
     async swap({
@@ -54,26 +63,30 @@ export class CetusSwapService {
         amount,
         a2b,
         slippage = 0.005, // 0.5%
-    }: SwapParams) {
+    }: SwapParams): Promise<SwapResult | undefined> {
         const maxCheck = 10
-        // check up to 10 times if swap is successful
         let ignoreFirst = true
+
         for (let i = 0; i < maxCheck; i++) {
             try {
-                const { balanceEnough } = await this.swapCore({ profilePair, amount, a2b, slippage })
-                this.logger.verbose(`Swap attempt #${i}`)
-                if (balanceEnough) {
+                const result = await this.swapCore({ profilePair, amount, a2b, slippage })
+                this.logger.verbose(`Swap attempt #${i}: ${JSON.stringify(result)}`)
+                if (result.balanceEnough && result.routeFound && result.digest) {
                     if (ignoreFirst) {
                         ignoreFirst = false
+                        await sleep(500)
                     } else {
-                        return
+                        return result
                     }
                 }
             } catch (error) {
-                this.logger.verbose(error)
+                this.logger.error(`Swap attempt #${i} failed`, error.message)
                 await sleep(500)
             }
         }
+
+        this.logger.warn(`Swap failed after ${maxCheck} attempts`)
+        return { balanceEnough: false, error: "Max attempts reached" }
     }
 
     async swapCore({
@@ -83,75 +96,80 @@ export class CetusSwapService {
         slippage = 0.001, // 0.1%
     }: SwapParams): Promise<SwapResult> {
         const pair = profilePair.pair as PairSchema
-        let rawAmount: BN
         const tokenA = pair.tokenA as TokenSchema
         const tokenB = pair.tokenB as TokenSchema
         const fromToken = a2b ? tokenA : tokenB
         const toToken = a2b ? tokenB : tokenA
+
+        // Amount to swap
+        let rawAmount: BN
         if (amount) {
             rawAmount = this.amountHelpersService.toRaw(fromToken.id, amount)
         } else {
-            const { displayId } = fromToken as TokenSchema
-            const { maxAmount } =
-        await this.balanceManagerService.calculateAvailableBalance(
-            displayId,
-            profilePair.capitalAllocatedMax,
-        )
+            const { displayId } = fromToken
+            const { maxAmount } = await this.balanceManagerService.calculateAvailableBalance(
+                displayId,
+                profilePair.capitalAllocatedMax,
+            )
             rawAmount = maxAmount
         }
+
         this.logger.warn(`Amount to swap: ${rawAmount.toString()}`)
+
+        // Check minimum amount
         if (rawAmount.lt(this.getMinAmountToSwap(fromToken.displayId))) {
             this.logger.warn(
                 `Not enough balance to swap ${rawAmount.toString()} ${fromToken.displayId}`,
             )
-            return {
-                balanceEnough: false,
-            }
+            return { balanceEnough: false, error: "Insufficient balance" }
         }
+
+        // Find router
         const tokenFrom = fromToken.address
         const tokenTarget = toToken.address
-        // router data v3
         this.logger.debug(
-            `[${pair.displayId}] Swapping ${rawAmount.toString()} ${tokenFrom} to ${tokenTarget}`,
+            `[${pair.displayId}] Swapping ${rawAmount.toString()} ${tokenFrom} -> ${tokenTarget}`,
         )
+
         const routerDataV3 = await this.cetusAggregatorSdk.findRouters({
             amount: rawAmount,
             byAmountIn: true,
             from: tokenFrom,
             target: tokenTarget,
         })
+
         if (!routerDataV3) {
-            this.logger.error(
-                `No router data v3 found for ${tokenFrom} to ${tokenTarget}`,
-            )
-            return {
-                balanceEnough: false
-            }
+            this.logger.error(`No router data v3 found for ${tokenFrom} -> ${tokenTarget}`)
+            return { balanceEnough: true, routeFound: false, error: "No route found" }
         }
+
+        // Build and execute transaction
         const txb = new Transaction()
         txb.setSender(envConfig().sui.walletAddress)
-        await this.cetusAggregatorSdk.fastRouterSwap({
-            router: routerDataV3,
-            slippage,
-            txb,
-        })
-        const signedTx = await this.suiClient.signAndExecuteTransaction({
-            transaction: txb,
-            signer: this.cetusSignerService.getSigner(),
-        })
-        if (signedTx?.digest) {
-            await this.suiClient.waitForTransaction({
-                digest: signedTx?.digest,
+
+        try {
+            await this.cetusAggregatorSdk.fastRouterSwap({
+                router: routerDataV3,
+                slippage,
+                txb,
             })
-            await this.cetusTxRateLimiterService.increaseTxCount()
-        }
-        this.logger.log(`Transaction digest: ${signedTx.digest}`)
-        return {
-            balanceEnough: true
+
+            const signedTx = await this.suiClient.signAndExecuteTransaction({
+                transaction: txb,
+                signer: this.cetusSignerService.getSigner(),
+            })
+
+            if (signedTx?.digest) {
+                await this.suiClient.waitForTransaction({ digest: signedTx.digest })
+                await this.cetusTxRateLimiterService.increaseTxCount()
+                this.logger.log(`Transaction success: ${signedTx.digest}`)
+                return { balanceEnough: true, routeFound: true, digest: signedTx.digest }
+            } else {
+                return { balanceEnough: true, routeFound: true, error: "No digest returned" }
+            }
+        } catch (error) {
+            this.logger.error("Transaction failed", error.message)
+            return { balanceEnough: true, routeFound: true, error: error.message }
         }
     }
-}
-
-export interface SwapResult {
-    balanceEnough?: boolean;
 }
